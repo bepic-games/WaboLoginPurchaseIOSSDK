bcbc4a0b1e1726b448687c1017f8da32428bd3a1
yilang <yilang@bepic.cc>
2023-02-17 14:31:46 +0800

    read to push

---
+++ WaboLoginPurchaseIOSSDK/WaboIap.framework/Headers/WaboIap.h
@@ -0,0 +1,18 @@
+//
+//  WaboIap.h
+//  WaboIap
+//
+//  Created by Bepic on 2023/1/13.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WaboIap.
+FOUNDATION_EXPORT double WaboIapVersionNumber;
+
+//! Project version string for WaboIap.
+FOUNDATION_EXPORT const unsigned char WaboIapVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WaboIap/PublicHeader.h>
+
+
---
+++ WaboLoginPurchaseIOSSDK/WaboLogin.framework/Headers/WaboLogin.h
@@ -0,0 +1,18 @@
+//
+//  WaboLogin.h
+//  WaboLogin
+//
+//  Created by Bepic on 2023/2/7.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WaboLogin.
+FOUNDATION_EXPORT double WaboLoginVersionNumber;
+
+//! Project version string for WaboLogin.
+FOUNDATION_EXPORT const unsigned char WaboLoginVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WaboLogin/PublicHeader.h>
+
+
