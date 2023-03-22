71f950513d4566fcf4b15993f8306d22a826be64
yilang <yilang@bepic.cc>
2023-03-22 15:30:43 +0800

    read to push

--- WaboLoginPurchaseIOSSDK.podspec
+++ WaboLoginPurchaseIOSSDK.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |spec|
   spec.name = "WaboLoginPurchaseIOSSDK"
-  spec.version="0.1.1"
+  spec.version="0.1.2"
   spec.summary = "Wabo SDK LoginPurchase sdk"
   spec.description= <<-DESC
   Wabo SDK for LoginPurchase
--- WaboLoginPurchaseIOSSDK/WaboLogin.framework/Headers/WaboLogin-Swift.h
+++ WaboLoginPurchaseIOSSDK/WaboLogin.framework/Headers/WaboLogin-Swift.h
@@ -282,6 +282,9 @@ SWIFT_CLASS("_TtC9WaboLogin9WaboLogin")
 /// returns:
 /// Void
 - (void)onInitWithJson:(NSString * _Nonnull)jsonString :(WaboStaticInfo * _Nonnull)staticInfo;
+- (WaboBridgeResult * _Nonnull)isLogined SWIFT_WARN_UNUSED_RESULT;
+- (WaboBridgeResult * _Nonnull)getGameAccountId SWIFT_WARN_UNUSED_RESULT;
+- (WaboBridgeResult * _Nonnull)getSessionToken SWIFT_WARN_UNUSED_RESULT;
 - (WaboBridgeResult * _Nonnull)getLoginTypes SWIFT_WARN_UNUSED_RESULT;
 - (void)getLoginParams:(NSString * _Nonnull)loginType :(void (^ _Nonnull)(id _Nullable))block;
 - (void)autoLoginAsync:(BOOL)needGameCenter :(void (^ _Nonnull)(id _Nullable))block;
